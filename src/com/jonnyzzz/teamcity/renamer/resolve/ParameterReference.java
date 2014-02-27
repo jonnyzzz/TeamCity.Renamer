3f69610aacca56f601ac4e3e4084c7a438849d64
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-02-28 00:05:55 +0100

    include properties from project files

--- src/com/jonnyzzz/teamcity/renamer/resolve/ParameterReference.java
+++ src/com/jonnyzzz/teamcity/renamer/resolve/ParameterReference.java
@@ -1,11 +1,17 @@
 package com.jonnyzzz.teamcity.renamer.resolve;
 
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReferenceBase;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.DomManager;
 import com.intellij.util.xml.GenericDomValue;
 import com.jonnyzzz.teamcity.renamer.model.ParameterElement;
 import com.jonnyzzz.teamcity.renamer.model.ParametersBlockElement;
+import com.jonnyzzz.teamcity.renamer.model.project.ProjectFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,6 +45,23 @@ public class ParameterReference extends PsiReferenceBase<PsiElement> {
           return element.getParameterName().getXmlAttributeValue();
         }
       }
+    }
+
+    final PsiDirectory parentDir = myElement.getContainingFile().getParent().getParentDirectory();
+    if (parentDir != null) {
+      final PsiFile projectFile = parentDir.findFile("project-config.xml");
+
+      if (projectFile != null && projectFile instanceof XmlFile) {
+        final DomElement projectXml = DomManager.getDomManager(myElement.getProject()).getDomElement(((XmlFile) projectFile).getRootTag());
+        if (projectXml != null && projectXml instanceof ProjectFile) {
+          for (ParameterElement element : ((ProjectFile) projectXml).getParametersBlock().getParameters()) {
+            if (myReferredVariableName.equals(element.getParameterName().getStringValue())) {
+              return element.getParameterName().getXmlAttributeValue();
+            }
+          }
+        }
+      }
+
     }
 
     return null;
