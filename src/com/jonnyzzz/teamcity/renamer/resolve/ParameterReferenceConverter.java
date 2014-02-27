de6d12da96d28ea90fbe9f65e21622394b84edf8
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-02-27 10:14:10 +0100

    report parameter references

--- src/com/jonnyzzz/teamcity/renamer/resolve/ParameterReferenceConverter.java
+++ src/com/jonnyzzz/teamcity/renamer/resolve/ParameterReferenceConverter.java
@@ -1,15 +1,16 @@
 package com.jonnyzzz.teamcity.renamer.resolve;
 
-import com.intellij.psi.ElementManipulators;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.util.xml.ConvertContext;
-import com.intellij.util.xml.Converter;
-import com.intellij.util.xml.CustomReferenceConverter;
-import com.intellij.util.xml.GenericDomValue;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.util.xml.*;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -17,8 +18,37 @@ import org.jetbrains.annotations.Nullable;
 public class ParameterReferenceConverter extends Converter<String> implements CustomReferenceConverter<String> {
   @NotNull
   @Override
-  public PsiReference[] createReferences(GenericDomValue<String> value, PsiElement element, ConvertContext context) {
-    return new PsiReference[] { new ParameterReference(value, element, ElementManipulators.getManipulator(element).getRangeInElement(element))};
+  public PsiReference[] createReferences(GenericDomValue<String> _value, PsiElement element, ConvertContext context) {
+    final GenericAttributeValue<String> value = (GenericAttributeValue<String>) _value;
+    final XmlAttributeValue xmlValue = value.getXmlAttributeValue();
+    if(xmlValue == null) return PsiReference.EMPTY_ARRAY;
+
+
+    int startOffset = xmlValue.getValueTextRange().getStartOffset() - xmlValue.getTextOffset() + 1;
+    final String text = value.getStringValue();
+
+    final List<ParameterReference> references = new ArrayList<ParameterReference>(0);
+
+    if (text == null) return PsiReference.EMPTY_ARRAY;
+    for(int idx = 0; idx < text.length(); idx++) {
+
+      if (text.charAt(idx) == '%') {
+        idx++;
+        final int refStart = idx + startOffset;
+
+        while (idx < text.length() && text.charAt(idx) != '%') idx++;
+        final int refEnd = idx + startOffset;
+
+        if (refStart < refEnd) {
+          references.add(new ParameterReference(value, xmlValue, new TextRange(refStart - 1, refEnd + 1)));
+        }
+
+        //skip last % char
+        idx++;
+      }
+    }
+
+    return references.toArray(new PsiReference[references.size()]);
   }
 
   @Nullable
