ae134c5b9dcfd5f0eedb9969c947ba25cce65d0e
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-02-27 20:53:34 +0100

    resolve tests added

---
+++ testSrc/com/jonnyzzz/teamcity/renamer/ParametersResolveTest.java
@@ -0,0 +1,37 @@
+package com.jonnyzzz.teamcity.renamer;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.testFramework.ResolveTestCase;
+import junit.framework.Assert;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ */
+public class ParametersResolveTest extends ResolveTestCase {
+
+  @Override
+  protected String getTestDataPath() {
+    return "testData/resolve/";
+  }
+
+  public void test_propertyResolve_001() throws Exception {
+    final PsiReference ref = configureByFile("BuildTypePropertyResolve_001.xml");
+    PsiElement resolved = ref.resolve();
+
+    Assert.assertTrue(resolved instanceof XmlAttributeValue && ((XmlAttributeValue) resolved).getValue().equals("ggg"));
+  }
+
+  public void test_propertyResolve_002() throws Exception {
+    final PsiReference ref = configureByFile("BuildTypePropertyResolve_002.xml");
+    PsiElement resolved = ref.resolve();
+
+    Assert.assertTrue(resolved instanceof XmlAttributeValue && ((XmlAttributeValue) resolved).getValue().equals("ggg"));
+  }
+
+
+
+
+
+}
