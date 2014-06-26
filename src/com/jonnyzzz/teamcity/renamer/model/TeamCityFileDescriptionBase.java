0c26f27c3a7ed5eb45a5c4188714e4c54b0e9e58
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-06-26 18:18:14 +0200

    report dependencies and prepare for scopes

--- src/com/jonnyzzz/teamcity/renamer/model/TeamCityFileDescriptionBase.java
+++ src/com/jonnyzzz/teamcity/renamer/model/TeamCityFileDescriptionBase.java
@@ -1,8 +1,16 @@
 package com.jonnyzzz.teamcity.renamer.model;
 
+import com.google.common.collect.ImmutableSet;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.DomFileDescription;
+import com.intellij.util.xml.GenericDomValue;
+import com.jonnyzzz.teamcity.renamer.resolve.Visitors;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -12,5 +20,38 @@ public abstract class TeamCityFileDescriptionBase<T extends TeamCityFile> extend
                                      @NotNull final @NonNls String rootTagName,
                                      @NotNull @NonNls final String... allPossibleRootTagNamespaces) {
     super(rootElementClass, rootTagName, allPossibleRootTagNamespaces);
+  }
+
+  @Override
+  public boolean hasStubs() {
+    return true;
+  }
+
+  @Override
+  public int getStubVersion() {
+    return getClass().getName().hashCode() & 0xffffff + 42;
+  }
+
+  @NotNull
+  @Override
+  public Set<TeamCityFile> getDependencyItems(XmlFile file) {
+    if (file == null) return Collections.emptySet();
+
+    final TeamCityFile tc = TeamCityFile.toTeamCityFile(TeamCityFile.class, file);
+    if (tc == null) return Collections.emptySet();
+
+    return ImmutableSet.<TeamCityFile>copyOf(Visitors.getProjectFiles(tc.getParentProjectFile()));
+  }
+
+  @NotNull
+  @Override
+  public DomElement getResolveScope(GenericDomValue<?> reference) {
+    return super.getResolveScope(reference);
+  }
+
+  @NotNull
+  @Override
+  public DomElement getIdentityScope(DomElement element) {
+    return super.getIdentityScope(element);
   }
 }
--- src/com/jonnyzzz/teamcity/renamer/model/buildType/BuildTypeDescription.java
+++ src/com/jonnyzzz/teamcity/renamer/model/buildType/BuildTypeDescription.java
@@ -1,6 +1,14 @@
 package com.jonnyzzz.teamcity.renamer.model.buildType;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.intellij.psi.xml.XmlFile;
+import com.jonnyzzz.teamcity.renamer.model.TeamCityFile;
 import com.jonnyzzz.teamcity.renamer.model.TeamCityFileDescriptionBase;
+import com.jonnyzzz.teamcity.renamer.model.template.BuildTemplateFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -8,5 +16,20 @@ import com.jonnyzzz.teamcity.renamer.model.TeamCityFileDescriptionBase;
 public class BuildTypeDescription extends TeamCityFileDescriptionBase<BuildTypeFile> {
   public BuildTypeDescription() {
     super(BuildTypeFile.class, "build-type");
+  }
+
+  @NotNull
+  @Override
+  public Set<TeamCityFile> getDependencyItems(XmlFile file) {
+    final Set<TeamCityFile> base = super.getDependencyItems(file);
+
+    final BuildTypeFile bt = TeamCityFile.toTeamCityFile(BuildTypeFile.class, file);
+
+    if (bt == null) return base;
+
+    final BuildTemplateFile baseTemplate = bt.getBaseTemplate();
+
+    if (baseTemplate == null) return base;
+    return Sets.union(ImmutableSet.of(baseTemplate), base);
   }
 }
