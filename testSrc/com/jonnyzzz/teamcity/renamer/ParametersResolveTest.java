383b843332dd04e319756ac805e6c6bbb6b4e558
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-06-27 01:02:51 +0200

    fix tests (only local)

--- testSrc/com/jonnyzzz/teamcity/renamer/ParametersResolveTest.java
+++ testSrc/com/jonnyzzz/teamcity/renamer/ParametersResolveTest.java
@@ -2,8 +2,8 @@ package com.jonnyzzz.teamcity.renamer;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.testFramework.ResolveTestCase;
+import com.jonnyzzz.teamcity.renamer.resolve.property.RenameableParameterElement;
 import org.junit.Assert;
 
 /**
@@ -20,18 +20,14 @@ public class ParametersResolveTest extends ResolveTestCase {
     final PsiReference ref = configureByFile("BuildTypePropertyResolve_001.xml");
     PsiElement resolved = ref.resolve();
 
-    Assert.assertTrue(resolved instanceof XmlAttributeValue && ((XmlAttributeValue) resolved).getValue().equals("ggg"));
+    Assert.assertTrue(resolved instanceof RenameableParameterElement && ((RenameableParameterElement) resolved).getName().equals("ggg"));
   }
 
   public void test_propertyResolve_002() throws Exception {
     final PsiReference ref = configureByFile("BuildTypePropertyResolve_002.xml");
     PsiElement resolved = ref.resolve();
 
-    Assert.assertTrue(resolved instanceof XmlAttributeValue && ((XmlAttributeValue) resolved).getValue().equals("ggg"));
+    Assert.assertTrue(resolved instanceof RenameableParameterElement && ((RenameableParameterElement) resolved).getName().equals("ggg"));
   }
-
-
-
-
 
 }
