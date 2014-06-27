952d0d4785bc024fdf5f43c5e8502235e9938aac
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2014-06-27 21:12:10 +0200

    meta runner fixes

--- src/com/jonnyzzz/teamcity/renamer/model/metaRunner/MetaRunnerFile.java
+++ src/com/jonnyzzz/teamcity/renamer/model/metaRunner/MetaRunnerFile.java
@@ -1,11 +1,14 @@
 package com.jonnyzzz.teamcity.renamer.model.metaRunner;
 
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.xml.*;
 import com.jonnyzzz.teamcity.renamer.model.SettingsElement;
 import com.jonnyzzz.teamcity.renamer.model.TeamCityFile;
 import com.jonnyzzz.teamcity.renamer.model.project.ProjectFile;
+import com.jonnyzzz.teamcity.renamer.resolve.property.DeclaredProperty;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -20,6 +23,27 @@ public abstract class MetaRunnerFile extends TeamCityFile {
   @Stubbed
   @SubTag("settings")
   public abstract SettingsElement getSettings();
+
+  @NotNull
+  @Override
+  public Iterable<DeclaredProperty> getDeclaredParameters() {
+    return getSettings().getParametersBlock().getDeclarations();
+  }
+
+  @Nullable
+  @Override
+  public String getFileId() {
+    final PsiFile containingFile = getContainingFile();
+    if (containingFile == null) return null;
+
+    return FileUtil.getNameWithoutExtension(containingFile.getName());
+  }
+
+  @NotNull
+  @Override
+  public String getFileKind() {
+    return "meta runner";
+  }
 
   @Override
   @Nullable
