370d1c1347fc28208a8ea0c62f7c845fb9eb417a
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2014-06-27 15:42:16 +0200

    Resolving for BuildType unified

--- src/com/jonnyzzz/teamcity/renamer/resolve/buildTypes/BuildTypeReference.java
+++ src/com/jonnyzzz/teamcity/renamer/resolve/buildTypes/BuildTypeReference.java
@@ -1,31 +1,21 @@
 package com.jonnyzzz.teamcity.renamer.resolve.buildTypes;
 
-import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReferenceBase;
 import com.intellij.psi.xml.XmlElement;
-import com.intellij.util.ArrayUtil;
 import com.intellij.util.xml.GenericDomValue;
 import com.jonnyzzz.teamcity.renamer.model.TeamCityFile;
 import com.jonnyzzz.teamcity.renamer.model.buildType.BuildTypeFile;
 import com.jonnyzzz.teamcity.renamer.model.project.ProjectFile;
 import com.jonnyzzz.teamcity.renamer.resolve.RenameableTeamCityFileElement;
+import com.jonnyzzz.teamcity.renamer.resolve.TeamCityFileReference;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+public class BuildTypeReference extends TeamCityFileReference<BuildTypeFile> {
 
-public class BuildTypeReference extends PsiReferenceBase<PsiElement> {
-
-  private final GenericDomValue<String> myAttr;
-
-  public BuildTypeReference(@NotNull GenericDomValue<String> attr, @NotNull PsiElement element) {
-    super(element);
-    myAttr = attr;
+  protected BuildTypeReference(@NotNull GenericDomValue<String> attr, @NotNull PsiElement element) {
+    super(attr, element);
   }
 
   @Nullable
@@ -49,31 +39,8 @@ public class BuildTypeReference extends PsiReferenceBase<PsiElement> {
     return new RenameableTeamCityFileElement(containingFile);
   }
 
-  @NotNull
   @Override
-  public Object[] getVariants() {
-    String value = myAttr.getValue();
-    if (value == null)
-      return ArrayUtil.EMPTY_OBJECT_ARRAY;
-
-    TeamCityFile file = myAttr.getParentOfType(TeamCityFile.class, false);
-    if (file == null)
-      return ArrayUtil.EMPTY_OBJECT_ARRAY;
-
-    ProjectFile projectFile = file.getParentProjectFile();
-    if (projectFile == null)
-      return ArrayUtil.EMPTY_OBJECT_ARRAY;
-
-    List<LookupElement> result = new ArrayList<>();
-    for (BuildTypeFile f : projectFile.getAllBuildTypes()) {
-      XmlElement xmlElement = f.getXmlElement();
-      if (xmlElement == null)
-        continue;
-
-      PsiFile containingFile = xmlElement.getContainingFile();
-      result.add(LookupElementBuilder.create(containingFile, FileUtil.getNameWithoutExtension(containingFile.getName()))
-              .withTypeText(FileUtil.getNameWithoutExtension(containingFile.getName())));
-    }
-    return result.toArray();
+  protected Iterable<BuildTypeFile> getAll(@NotNull ProjectFile projectFile) {
+    return projectFile.getAllBuildTypes();
   }
 }
--- src/com/jonnyzzz/teamcity/renamer/resolve/buildTypes/BuildTypeReferenceConverter.java
+++ src/com/jonnyzzz/teamcity/renamer/resolve/buildTypes/BuildTypeReferenceConverter.java
@@ -1,34 +1,14 @@
 package com.jonnyzzz.teamcity.renamer.resolve.buildTypes;
 
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.xml.XmlAttributeValue;
-import com.intellij.util.xml.*;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.util.xml.GenericDomValue;
+import com.jonnyzzz.teamcity.renamer.resolve.TeamCityFileReferenceConverter;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class BuildTypeReferenceConverter extends Converter<String> implements CustomReferenceConverter<String> {
+public class BuildTypeReferenceConverter extends TeamCityFileReferenceConverter<BuildTypeReference> {
 
-  @NotNull
   @Override
-  public PsiReference[] createReferences(GenericDomValue<String> value, PsiElement element, ConvertContext context) {
-    GenericAttributeValue<String> val = (GenericAttributeValue<String>) value;
-    XmlAttributeValue xmlValue = val.getXmlAttributeValue();
-    if(xmlValue == null) return PsiReference.EMPTY_ARRAY;
-
-    return new PsiReference[]{new BuildTypeReference(val, element)};
-  }
-
-  @Nullable
-  @Override
-  public String fromString(@Nullable @NonNls String s, ConvertContext context) {
-    return s;
-  }
-
-  @Nullable
-  @Override
-  public String toString(@Nullable String s, ConvertContext context) {
-    return s;
+  protected BuildTypeReference createReference(@NotNull GenericDomValue<String> attr, @NotNull PsiElement element) {
+    return new BuildTypeReference(attr, element);
   }
 }
