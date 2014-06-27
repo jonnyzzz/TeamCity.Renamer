ec01bb2f2243c4880239229441e649082b4b7bad
Dmitry Neverov <dmitry.neverov@gmail.com>
2014-06-27 17:55:04 +0200

    d'n'd for dirs

--- src/com/jonnyzzz/teamcity/renamer/diagram/TCDnDProvider.java
+++ src/com/jonnyzzz/teamcity/renamer/diagram/TCDnDProvider.java
@@ -2,10 +2,16 @@ package com.jonnyzzz.teamcity.renamer.diagram;
 
 import com.intellij.diagram.extras.providers.DiagramDnDProvider;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiFile;
 import com.jonnyzzz.teamcity.renamer.model.TeamCityFile;
 import com.jonnyzzz.teamcity.renamer.model.TeamCitySettingsBasedFile;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TCDnDProvider implements DiagramDnDProvider<TCElement> {
   @Override
@@ -16,6 +22,28 @@ public class TCDnDProvider implements DiagramDnDProvider<TCElement> {
   @Nullable
   @Override
   public TCElement[] wrapToModelObject(Object o, Project project) {
+    if (o instanceof PsiDirectory) {
+      PsiDirectory dir = ((PsiDirectory) o);
+      final List<PsiFile> files = new ArrayList<>();
+      dir.acceptChildren(new PsiElementVisitor() {
+        @Override
+        public void visitFile(PsiFile file) {
+          files.add(file);
+        }
+
+        @Override
+        public void visitDirectory(PsiDirectory dir) {
+          dir.acceptChildren(this);
+        }
+      });
+      List<TCElement> result = new ArrayList<>();
+      for (PsiFile f : files) {
+        TeamCitySettingsBasedFile tcFile = TeamCityFile.toTeamCityElement(TeamCitySettingsBasedFile.class, f);
+        if (tcFile != null)
+          result.add(new TCElement(tcFile));
+      }
+      return result.toArray(new TCElement[result.size()]);
+    }
     if (o instanceof PsiElement) {
       TeamCitySettingsBasedFile file = TeamCityFile.toTeamCityElement(TeamCitySettingsBasedFile.class, (PsiElement) o);
       if (file != null)
