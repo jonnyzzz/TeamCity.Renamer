89f70c9363c7eb08be9af70b8c4a7cf18f2b9275
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2014-06-27 23:52:40 +0200

    Change to quick fixes

--- src/com/jonnyzzz/teamcity/renamer/resolve/property/DefineSettingsParameter.java
+++ src/com/jonnyzzz/teamcity/renamer/resolve/property/DefineSettingsParameter.java
@@ -2,22 +2,22 @@ package com.jonnyzzz.teamcity.renamer.resolve.property;
 
 import com.jonnyzzz.teamcity.renamer.model.ParameterElement;
 import com.jonnyzzz.teamcity.renamer.model.ParametersBlockElement;
-import com.jonnyzzz.teamcity.renamer.model.project.ProjectFile;
+import com.jonnyzzz.teamcity.renamer.model.SettingsElement;
 import org.jetbrains.annotations.NotNull;
 
-public class DefineProjectParameter extends AbstractDefineNewParameterQuickFix {
-  private final ProjectFile myProjectFile;
+public class DefineSettingsParameter extends AbstractDefineNewParameterQuickFix {
+  private final SettingsElement mySettings;
   private final String myText;
 
-  public DefineProjectParameter(String name, ProjectFile projectFile, String text) {
+  public DefineSettingsParameter(String name, SettingsElement settings, String text) {
     super(name);
-    myProjectFile = projectFile;
+    mySettings = settings;
     myText = text;
   }
 
   @Override
   protected ParametersBlockElement getWhereToPlace(ParameterElement element) {
-    return myProjectFile.getParametersBlock();
+    return mySettings.getParametersBlock();
   }
 
   @NotNull
